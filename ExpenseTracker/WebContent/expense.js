2ae2e0d554cf9905bcca9da148f885ed28bef447
Jonathan Edwards <edwardjd@colorado.edu>
2016-12-18 11:24:19 -0700

    jquery to build table with data provided by ajax request.

--- ExpenseTracker/WebContent/expense.js
+++ ExpenseTracker/WebContent/expense.js
@@ -1,3 +1,49 @@
 $(function() {
-  console.log("test");
+	start();
 });
+
+var start = function() {
+	$.ajax({
+		type : "GET",
+		url : "api/expense",
+		dataType : "json",
+		success : printTable
+	})
+
+};
+
+var printTable = function(data) {
+	$table = $('<table>');
+	$row = $('<tr>');
+	$th1 = $('<th>');
+	$th2 = $('<th>');
+	$th3 = $('<th>');
+	$th4 = $('<th>');
+	$th5 = $('<th>');
+
+	$th1.text("id");
+	$th2.text("title");
+	$th3.text("description");
+	$th4.text("amount");
+	$th5.text("category");
+
+	$row.append($th1, $th2, $th3, $th4, $th5);
+	$table.append($row);
+	$('body').append($table);
+
+	populateTable(data);
+};
+
+var populateTable = function(data) {
+	$(data).each(function() {
+		$row = $('<tr>');
+		$table.append($row);
+
+		for ( var key in this) {
+			$column = $('<td>');
+			$column.text(this[key]);
+			$row.append($column);
+
+		}
+	});
+}
