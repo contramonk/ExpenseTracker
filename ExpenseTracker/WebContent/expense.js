5b2169b9e5fc95ec5d741c1c4f49eb6e53f72a32
Jonathan Edwards <edwardjd@colorado.edu>
2016-12-18 21:56:42 -0700

    add delete, create, and edit functionality.

--- ExpenseTracker/WebContent/expense.js
+++ ExpenseTracker/WebContent/expense.js
@@ -3,6 +3,7 @@ $(function() {
 });
 
 var start = function() {
+	$('body').text("");
 	$.ajax({
 		type : "GET",
 		url : "api/expense",
@@ -13,6 +14,7 @@ var start = function() {
 };
 
 var printTable = function(data) {
+	$createButton = $('<button>')
 	$table = $('<table>');
 	$thead = ('<thead>')
 	$row = $('<tr>');
@@ -22,6 +24,7 @@ var printTable = function(data) {
 	$th4 = $('<th>');
 	$th5 = $('<th>');
 
+	$createButton.text('add');
 	$th1.text("id");
 	$th2.text("title");
 	$th3.text("description");
@@ -30,9 +33,72 @@ var printTable = function(data) {
 
 	$row.append($th1, $th2, $th3, $th4, $th5);
 	$table.append($row);
+	$('body').append($createButton);
 	$('body').append($table);
+	
+	$createButton.click(function() {createEvent()});
 
 	populateTable(data);
+};
+
+var createEvent = function() {
+	$('form').text("");
+	
+	//put into method
+	$addForm = $('<form>');
+	$idLabel = $('<label>');
+	$titleLabel = $('<label>');
+	$descriptionLabel = $('<label>');
+	$amountLabel = $('<label>');
+	$categoryLabel = $('<label>');
+	$addSubmit = $('<button>')
+	
+	$idInput = $('<input>');
+	$titleInput = $('<input>');
+	$descriptionInput = $('<input>');
+	$amountInput = $('<input>');
+	$categoryInput = $('<input>');
+	
+	$idLabel.text('id: ');
+	$titleLabel.text('title: ');
+	$descriptionLabel.text('description: ');
+	$amountLabel.text('amount: ');
+	$categoryLabel.text('category: ');
+	$addSubmit.text('submit');
+	
+	$addForm.append($idLabel, $idInput, $titleLabel, $titleInput,
+			$descriptionLabel, $descriptionInput, $amountLabel, $amountInput,
+			$categoryLabel, $categoryInput, $addSubmit);
+	$('body').append($addForm);
+	
+	$addSubmit.click(function(e) {addEvent(e, $titleInput.val(), $descriptionInput.val(), $amountInput.val(), $categoryInput.val())});
+	
+}
+
+var addEvent = function(e, $titleInput, $descriptionInput, $amountInput, $categoryInput) {
+	e.preventDefault();
+	
+	var newEvent = {
+			title: $titleInput,
+			description: $descriptionInput,
+			amount: $amountInput,
+			category: $categoryInput	
+	};
+	
+	var jsonStringy = JSON.stringify(newEvent);
+	
+	$.ajax({
+		type : "POST",
+		url : "api/expense",
+		// why did i have to use contentType here, but not in editEvent?
+		contentType : "application/json; charset=utf-8",
+		data : jsonStringy,
+		dataType : "json",
+		success : start
+	});
+	
+	
+	
 };
 
 var populateTable = function(data) {
@@ -54,25 +120,28 @@ var populateTable = function(data) {
 		var description = this.description;
 		var amount = this.amount;
 		var category = this.category;
-//		console.log(this.id);
-//		console.log(this.title);
-//		console.log(this.category);
+
 		$editButton.click(function(e) {
 			editEvent(e, id, title, description, amount, category)
 			});
-		$deleteButton.click(deleteEvent);
+		$deleteButton.click(function(e) {deleteEvent(e, id)});
 		$row.append($editButton, $deleteButton);
 
 	});
 }
 
+
+
 var editEvent = function(e, id, title, description, amount, category) {
+	
+	// put into method
 	$editForm = $('<form>');
 	$idLabel = $('<label>');
 	$titleLabel = $('<label>');
 	$descriptionLabel = $('<label>');
 	$amountLabel = $('<label>');
 	$categoryLabel = $('<label>');
+	$editSubmit = $('<button>')
 	
 	$idInput = $('<input>');
 	$titleInput = $('<input>');
@@ -92,23 +161,44 @@ var editEvent = function(e, id, title, description, amount, category) {
 	$descriptionLabel.text('description: ');
 	$amountLabel.text('amount: ');
 	$categoryLabel.text('category: ');
+	$editSubmit.text('submit');
 	
 	
 	
 	$editForm.append($idLabel, $idInput, $titleLabel, $titleInput,
 			$descriptionLabel, $descriptionInput, $amountLabel, $amountInput,
-			$categoryLabel, $categoryInput);
+			$categoryLabel, $categoryInput, $editSubmit);
 	$('body').append($editForm);
 	
+	$editSubmit.click(function(e) {
+		e.preventDefault;
+		
+		var updatedEvent = {
+				title: $titleInput.val(),
+				description: $descriptionInput.val(),
+				amount: $amountInput.val(),
+				category: $categoryInput.val()	
+		}
+		
+		var jsonStringy = JSON.stringify(updatedEvent);
+		
+		
+		$.ajax({
+			type : "PUT",
+			url : "api/expense/" + id,
+			data: jsonStringy,
+			dataType : "json",
+			success : printTable
+		})
+	});
 	
-//	$.ajax({
-//		type : "PUT",
-//		url : "api/expense/" + id,
-//		dataType : "json",
-//		success : printTable
-//	})
 };
 
-var deleteEvent = function() {
-	console.log('click');
+var deleteEvent = function(e, id) {
+	$.ajax({
+		type : "DELETE",
+		url : "api/expense/" + id,
+		dataType : "json",
+		success : start
+	})
 };
